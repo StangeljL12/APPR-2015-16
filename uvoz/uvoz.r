3f5dae90e01ef42d4e10beb4cf4ab68bcf765eed
Laura Štangelj <stangelj.laura@gmail.com>
2015-12-23 08:31:03 +0100

    uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,34 @@
-# 2. faza: Uvoz podatkov
+#tabela po mesecih
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+uvozi.PoMesecih <- function() {
+  return(read.csv2("podatki/PoMesecih.csv", sep = ";", as.is = TRUE,
+                   #row.names = 1, 
+                   skip = 2,
+                   col.names=c("Leto", "SKUPAJ", "Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December"),
+                   fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
 
-obcine <- uvozi.obcine()
+PoMesecih <- uvozi.PoMesecih()
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+#tabela po regiji
+
+uvozi.PoRegijiPrebivalisca2014 <- function() {
+  return(read.csv2("podatki/PoRegijiPrebivalisca2014.csv", sep = ";", as.is = TRUE,
+                   row.names = 1, skip = 4,
+                   fileEncoding = "Windows-1250"))
+}
+
+
+PoRegijiPrebivalisca2014 <- uvozi.PoRegijiPrebivalisca2014()
+
+#tabela po starosti
+
+uvozi.PoStarostnihSkupinah2014 <- function() {
+  return(read.csv2("podatki/PoStarostnihSkupinah2014.csv", sep = ";", as.is = TRUE,
+                   row.names = 1, skip = 4,
+                   fileEncoding = "Windows-1250"))
+}
+
+
+PoStarostnihSkupinah2014 <- uvozi.PoStarostnihSkupinah2014()
