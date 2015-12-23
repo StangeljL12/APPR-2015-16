b968108eee398fbbb92e29b32ccd1cd47979ae78
Laura Štangelj <stangelj.laura@gmail.com>
2015-12-23 08:32:54 +0100

    lib

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,4 +1,10 @@
-library(knitr)
+library(knitr) 
+library(ggplot2) 
+require(dplyr) 
+require(rvest) 
+require(gsubfn) 
+
+
 
 # Uvozimo funkcije za delo z datotekami XML.
 source("lib/xml.r", encoding = "UTF-8")
