d64a4611bce461ccc9d73c21dad80d73ba5d1cf3
Vesna <vesnazupanc93@gmail.com>
2016-09-11 22:04:08 +0200

    dodane knjižnjice

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,10 +1,17 @@
 library(knitr) 
 library(ggplot2) 
 require(dplyr) 
-require(rvest) 
+library(rvest) 
 require(gsubfn) 
-
-
+library(maptools)
+library(sp)
+library(digest)
+library(MASS)
+require(zoo)
+library(mgcv)
+library(magrittr)
+library(httr)
+library(scales)
 
 # Uvozimo funkcije za delo z datotekami XML.
 source("lib/xml.r", encoding = "UTF-8")
